b92acc721dc3b74bec3161f2f722156f34f7241a
Sdrtmr <150820654+Sdrtmr@users.noreply.github.com>
2024-06-04 22:25:56 +0300

    Update Autosite code.py

--- Autosite code.py
+++ Autosite code.py
@@ -1,145 +1,38 @@
 import psycopg2
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from flask import Flask
-import psycopg2
-
+import webbrowser
 from app import connect_to_db
 
 app = Flask(__name__)
 
-# Функция для генерации HTML кода с заголовком, кнопками и формой для ввода
-def generate_html_with_form(title):
-    return f"""
-    <html>
-    <head>
-        <title>{title}</title>
-        <meta charset="utf-8">
-        <style>
-            body {{
-                background-color: #333; /* Тёмно-серый цвет фона */
-                color: #fff; /* Цвет текста */
-            }}
-            h1, .catalog-title {{
-                background-color: white; /* Белый цвет фона для заголовка */
-                color: black; /* Черный цвет текста для заголовка */
-                padding: 10px;
-                text-align: center; /* Выравниваем заголовок по центру */
-            }}
-            .catalog-title {{
-                text-align: left; /* Выравниваем заголовок влево */
-            }}
-            a {{
-                color: white; /* Белый цвет текста для кнопок */
-                text-decoration: none; /* Убираем подчеркивание у ссылок */
-                margin-right: 0.5cm; /* Отступ между кнопками - 0.5 см */
-            }}
-            .button-container {{
-                text-align: center; /* Центрируем элементы внутри div по горизонтали */
-            }}
-            .popup {{
-                display: none;
-                position: fixed;
-                width: 80%;
-                height: 80%;
-                top: 10%;
-                left: 10%;
-                background-color: #001f3f; /* Тёмно-синий цвет фона для всплывающего окна */
-                color: white; /* Белый цвет текста во всплывающем окне */
-                padding: 20px;
-                z-index: 9999;
-                font-size: 12pt; /* Размер шрифта 12 пунктов */
-            }}
-        </style>
-    </head>
-    <body style="text-align:center;">
-        <h1 style="text-align:center;">{title}</h1>
-        <div class="button-container">
-            <a href="#">Главная</a>
-            <a href="/catalog">Каталог</a>
-            <a href="#" onclick="document.getElementById('popup').style.display='block';">О нас</a>
-            <a href="#">Корзина</a>
-        </div>
-            </form>
-        </div>
-        <div class="popup" id="popup" onclick="this.style.display='none';">
-            <p>Здравствуйте! Данный сайт является официальным интернет-магазином автосалона Cadia.<br>
-            Наш автосалон предлагает широкий выбор автомобилей под любые ваши нужды.<br>
-            Мы постарались сделать интернет-магазин наиболее удобным, чтобы вы могли найти подходящий лично вам автомобиль.</p>
-        </div>
-    </body>
-    </html>
-    """
 
-# Функция для генерации HTML кода для страницы с товарами
-def generate_catalog_page():
-    return """
-    <html>
-    <html>
-    <head>
-        <title>Каталог</title>
-        <meta charset="utf-8">
-        <style>
-            body {
-                background-color: #333; /* Тёмно-серый цвет фона */
-                color: white; /* Цвет текста */
-            }
-            .catalog-title {
-                background-color: white; /* Белый цвет фона для заголовка */
-                color: black; /* Черный цвет текста для заголовка */
-                padding: 10px;
-                text-align: left; /* Выравниваем заголовок влево */
-            }
-        </style>
-    </head>
-    <body>
-        <h1 class="catalog-title">Товары</h1>
-        <p>Вот что мы можем вам предложить.</p>
-    </body>
-</style>
-</head>
-<body>
-    <div class="row">
-        <h1>Товары в наличии</h1>
-        <p>Выбранный товар</p>
-<div class="col-md-4">
-    <div class="product">
-        <img src="Cars/car-1.png">
-        <div class="product_info">
-            <h3>Модель: AUDI A4</h3>
-            <p>Цена: 33 178 - 44 707 USD</p>
-            <a href="#Mymodal{{product[1]}}" class="click_to_button">Добавить в корзину</a>
-        </div>
-    </div>
-</div>
-<!-- Создаём модальное окно для оформления заказа! -->
-<div id="Mymodal{{ product[0] }}" class="modal">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="orderModalLabel">Оформление заказа</h5>
-            <a href="#" class="close">&times;</a>
-        </div>
-        <div class="modal-body">
-            <form id="orderForm" action="/submit_order" method="POST">
-                <input type="hidden" name="productId" value="{{ product[0] }}">
-                <div class="form-group">
-                    <label for="phoneNumber">Телефон</label>
-                    <input type="text" class="form-control" id="phoneNumber" name="phoneNumber" required>
-                </div>
-                <button type="submit" class="btn btn-primary">Завершить оформление заказа</button>
-            </form>
-        </div>
-    </div>
-</div>
-</div>
-</body>
-</html>
-<html>
-    """
+def open_html_file_in_chrome(file_path):
+  """
+  Открывает HTML файл в Google Chrome.
+
+  Args:
+    file_path: Путь к HTML файлу.
+  """
+  try:
+    # Получение пути к исполняемому файлу Chrome
+    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s' 
+    webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+    webbrowser.get('chrome').open_new_tab(f"file://{file_path}")
+    print(f"Файл '{file_path}' открыт в Google Chrome.")
+  except FileNotFoundError:
+    print(f"Ошибка: Файл '{file_path}' не найден.")
+  except Exception as e:
+    print(f"Ошибка: Не удалось открыть файл в Google Chrome. {e}")
+
+# Пример использования:
+file_to_open = "test_2.html"  
+open_html_file_in_chrome(file_to_open)
 
 # Функция для установки соединения с базой данных
 def connect_to_database():
     conn = psycopg2.connect(
-        dbname="Autosite-base",
+        dbname="Название базы данных",
         user="postgres",
         password="Пароль",
         host="localhost",
@@ -173,28 +66,6 @@ def execute_query(query):
     result = cur.fetchall()
     conn.close()
     return result
-# Создаем класс, унаследованный от BaseHTTPRequestHandler
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    # Определяем метод для обработки GET запросов
-    def do_GET(self):
-        self.send_response(200)  # Отправляем код успешного ответа
-        self.send_header('Content-type', 'text/html; charset=utf-8')  # Указываем тип контента как HTML с кодировкой UTF-8
-        self.end_headers()
-        
-        if self.path == '/':  # Если запрошен корневой путь, отображаем основную страницу
-            content = generate_html_with_form("Autosite-Cadia")
-        elif self.path == '/home':  # Если запрошен путь /home, отображаем страницу "Главная"
-            self.send_response(301)
-            self.send_header('Location', '/')
-            self.end_headers()
-            return
-        elif self.path == '/catalog':  # Если запрошен путь /catalog, отображаем страницу с товарами
-            content = generate_catalog_page()
-        else:
-            self.send_error(404, "Страница не найдена")
-            return
-        
-        self.wfile.write(content.encode('utf-8'))  # Отправляем HTML содержимое на страницу с указанием кодировки
 
 # Определяем IP адрес и порт сервера
 server_address = ('127.0.0.5', 9000)
